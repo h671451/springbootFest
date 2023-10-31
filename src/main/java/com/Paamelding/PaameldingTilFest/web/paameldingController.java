31b143aad2bf91ba410bab020aeb274878927561
dat20 <huynh123@live.no>
2023-10-31 21:35:07 +0100

    Nye endringer

--- src/main/java/com/Paamelding/PaameldingTilFest/web/paameldingController.java
+++ src/main/java/com/Paamelding/PaameldingTilFest/web/paameldingController.java
@@ -10,10 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class paameldingController {
     //@Autowired private PaameldingService paameldingService;
-
-    @RequestMapping("/paamelding")
+    @GetMapping("/")
+    public String index() {
+        return "index";  // this should match the name of your JSP file without the .jsp extension
+    }
+    @GetMapping("/paamelding")
     public String paameldingSkjema() {
         return "paamelding";
+    }
+
+    @GetMapping("/innlogging")
+    public String innloggingSkjema() {
+        return "innlogging";
+    }
+
+    @GetMapping("/paameldt")
+    public String paameldtSkjema() {
+        return "paameldt";
+    }
+
+    @GetMapping("/deltagerliste")
+    public String deltagerlisteSkjema() {
+        return "deltagerliste";
     }
 
 
--- src/main/webapp/WEB-INF/jsp/paameldt.jsp
+++ src/main/webapp/WEB-INF/jsp/paameldt.jsp
@@ -1,8 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-	<link href="css/simple.css" rel="stylesheet" type="text/css" />
+	<link href="/simple.css" rel="stylesheet" type="text/css" />
 <title>Påmeldingsbekreftelse</title>
 </head>
 <body>
